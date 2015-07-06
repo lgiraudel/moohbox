a0c63909e47b4c02f666393516c13e573aa92c05
Loic Giraudel <l.giraudel@gmail.com>
2015-07-06 18:59:55 +0200

    Init

---
+++ index.js
@@ -0,0 +1,15 @@
+var msg = 'Mooooh !';
+
+if (process.argv.length > 2) {
+  msg = process.argv[2];
+}
+
+console.log('')
+console.log('             ' + msg);
+console.log('')
+console.log('         (__)');
+console.log('         (oo)');
+console.log('  /-------\\/');
+console.log(' / |     ||');
+console.log('*  ||----||');
+console.log('   ^^    ^^');
